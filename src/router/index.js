eb3121e2ebdcafc5989732783607812a24fb57fe
meng <meng.liu1@bthome.com>
2025-06-05 14:35:03 +0800

    修改路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -94,13 +94,30 @@ const routers = createRouter({
  * 重置路由
  */
 export function resetRouter() {
-  const newRouter = createRouter({
-    history: createWebHashHistory(),
-    scrollBehavior: () => ({ left: 0, top: 0 }),
-    strict: true,
-    routes: [],
-  });
-  router.matcher = newRouter.matcher;
+  // const newRouter = createRouter({
+  //   history: createWebHashHistory(),
+  //   scrollBehavior: () => ({ left: 0, top: 0 }),
+  //   strict: true,
+  //   routes: [],
+  // });
+  routers.clearRoutes();
+  // routers.addRoute({});
 }
+
+// 全局前置守卫
+routers.beforeEach((to, from, next) => {
+  console.log("路由变化", to.path, from.path);
+
+  const list = routers.getRoutes();
+  if (list.length > 0) {
+    // 遍历路由列表，检查是否存在当前路由
+    const exists = list.some((route) => route.path === to.path);
+    if (!exists) {
+      console.warn(`路由 ${to.path} 不存在`);
+      return next({ name: "NotFound" });
+    }
+  }
+  next();
+});
 
 export default routers;
